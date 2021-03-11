a5c8de2df1b6af60db27e6c787a7a601d5b58b71
victor <victor-gr7@hotmail.com>
2021-03-11 23:55:46 +0100

    11/03/21

--- utils.py
+++ utils.py
@@ -38,7 +38,7 @@ def get_dataset(tokenizer, dataset_path, dataset_cache=None):
         dataset = torch.load(dataset_cache) 
     else:
         logger.info("Download dataset from %s", dataset_path)
-    #personachat_file = cached_path(dataset_path)
+        personachat_file = cached_path(dataset_path)
         with open('data_personachat.json', "r", encoding="utf-8") as f:
             dataset = json.loads(f.read())
 
