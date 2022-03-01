7c70cdac055899a7482c4e176e9ad9c5782d1706
victor <victor-gr7@hotmail.com>
2022-03-01 10:11:17 +0100

    faiss interact eval

--- interact_faiss.py
+++ interact_faiss.py
@@ -253,7 +253,7 @@ def run():
         history.append(out_ids)
         history = history[-(2*args.max_history+1):]
         out_text = tokenizer.decode(out_ids, skip_special_tokens=True)
-        print("personalidad: " + personality)
+        print(personality_decoded)
         print("Selected personality" + selected_personality)
         print("Chatbot's turn:" + out_text)
 
