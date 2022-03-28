8ca258b5a871e56c5317cbcf4d1815ba42854ba0
victor <victor-gr7@hotmail.com>
2022-03-29 00:56:36 +0200

    pegasus

--- pegasus_fine_tuning.py
+++ pegasus_fine_tuning.py
@@ -126,7 +126,7 @@ def prepare_data(model_name,
   return train_dataset, val_dataset, test_dataset, tokenizer
 
 
-def prepare_fine_tuning(model_name, tokenizer, train_dataset, val_dataset=None, freeze_encoder=False, output_dir='./results'):
+def prepare_fine_tuning(model_name, tokenizer, train_dataset, val_dataset=None, freeze_encoder=False, output_dir='./results1'):
   """
   Prepare configurations and base model for fine-tuning
   """
@@ -149,7 +149,7 @@ def prepare_fine_tuning(model_name, tokenizer, train_dataset, val_dataset=None, 
       eval_steps=100,                  # number of update steps before evaluation
       warmup_steps=500,                # number of warmup steps for learning rate scheduler
       weight_decay=0.01,               # strength of weight decay
-      logging_dir='./logs',            # directory for storing logs
+      logging_dir='./logs1',            # directory for storing logs
       logging_steps=10,
     )
 
