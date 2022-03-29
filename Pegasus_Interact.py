2d8db53128649c66b2bdce7f0a587f749085efd3
victor <victor-gr7@hotmail.com>
2022-03-29 17:17:24 +0200

    pegasus interact

--- Pegasus_Interact.py
+++ Pegasus_Interact.py
@@ -33,13 +33,49 @@ from transformers import cached_path
 tokenizer = PegasusTokenizer.from_pretrained("results1/checkpoint-95500/")
 model = PegasusForConditionalGeneration.from_pretrained("results1/checkpoint-95500/") 
 model.to("cpu")
+
+
+
+''' def sample_sequence(personality, history, tokenizer, model, args, current_output=None):
+    special_tokens_ids = tokenizer.convert_tokens_to_ids(SPECIAL_TOKENS)
+    if current_output is None:
+        current_output = []
+
+    for i in range(args.max_length):
+        instance = build_input_from_segments(personality, history, current_output, tokenizer, with_eos=False)
+
+        input_ids = torch.tensor(instance["input_ids"], device=args.device).unsqueeze(0)
+        token_type_ids = torch.tensor(instance["token_type_ids"], device=args.device).unsqueeze(0)
+
+        logits = model(input_ids, token_type_ids=token_type_ids)
+        if isinstance(logits, tuple):  # for gpt2 and maybe others
+            logits = logits[0]
+        logits = logits[0, -1, :] / args.temperature
+        logits = top_filtering(logits, top_k=args.top_k, top_p=args.top_p)
+        probs = F.softmax(logits, dim=-1)
+
+        prev = torch.topk(probs, 1)[1] if args.no_sample else torch.multinomial(probs, 1)
+        if i < args.min_length and prev.item() in special_tokens_ids:
+            while prev.item() in special_tokens_ids:
+                if probs.max().item() == 1:
+                    warnings.warn("Warning: model generating special token with probability 1.")
+                    break  # avoid infinitely looping over special token
+                prev = torch.multinomial(probs, num_samples=1)
+
+        if prev.item() in special_tokens_ids:
+            break
+        current_output.append(prev.item())
+
+    return current_output '''
+
 while True:
     raw_text = input(">>> ")
     while not raw_text:
         print('Prompt should not be empty!')
         raw_text = input(">>> ")
-    with torch.no_grad():
-        batch = tokenizer.prepare_seq2seq_batch(raw_text, truncation=True, padding='longest')
-        translated = model.generate(**batch)
-        tgt_text = tokenizer.batch_decode(translated, skip_special_tokens=True)
+    #batch = tokenizer.prepare_seq2seq_batch(raw_text, truncation=True, padding='longest')
+    batch = tokenizer(raw_text, truncation=True, padding="longest", return_tensors="pt").to('cpue'')
+    translated = model.generate(**batch)
+    tgt_text = tokenizer.batch_decode(translated, skip_special_tokens=True)
+    #tgt_text = tokenizer.batch_decode(translated, skip_special_tokens=True)
     print(tgt_text)
