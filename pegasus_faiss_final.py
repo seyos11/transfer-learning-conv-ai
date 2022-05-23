db017e17c24d87d58e70262be7ac301f8fc33631
victor <victor-gr7@hotmail.com>
2022-05-23 09:27:40 +0200

    1 sentence

--- pegasus_faiss_final.py
+++ pegasus_faiss_final.py
@@ -535,8 +535,8 @@ if __name__=='__main__':
   model_name_1 = './result_final_normal_4x4/' + files[0]
 
 
-  files = os.listdir('result_final_normal_3x3/')
-  model_name_4 = './result_final_faiss_3x3' + files[0]
+  #files = os.listdir('result_final_normal_3x3/')
+  #model_name_4 = './result_final_faiss_3x3' + files[0]
   #train_dataset, valid_dataset, _, tokenizer = prepare_data(model_name, train_texts, train_labels,val_texts=valid_texts, val_labels=valid_labels)
   #trainer = prepare_fine_tuning(model_name, tokenizer, train_dataset,val_dataset=valid_dataset)
   #trainer.train()
