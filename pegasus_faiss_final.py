c7e9b558d4192c53d5e768d62e74702da29097d6
victor <victor-gr7@hotmail.com>
2022-05-24 08:12:26 +0200

    final

--- pegasus_faiss_final.py
+++ pegasus_faiss_final.py
@@ -576,9 +576,9 @@ if __name__=='__main__':
   #dataset = load_dataset("xsum")
   train_texts, train_labels = dataset['train']['input_ids'], dataset['train']['decoder_input_ids']
   valid_texts, valid_labels = dataset['valid']['input_ids'], dataset['valid']['input_ids']
-  train_dataset, valid_dataset, _, tokenizer = prepare_data(model_name_1, train_texts, train_labels,val_texts=valid_texts, val_labels=valid_labels)
-  trainer2 = prepare_fine_tuning_faiss1x1(model_name_1, tokenizer, train_dataset,val_dataset=valid_dataset)
-  trainer2.train()
+  #train_dataset, valid_dataset, _, tokenizer = prepare_data(model_name_1, train_texts, train_labels,val_texts=valid_texts, val_labels=valid_labels)
+  #trainer2 = prepare_fine_tuning_faiss1x1(model_name_1, tokenizer, train_dataset,val_dataset=valid_dataset)
+  #trainer2.train()
   
   files = os.listdir('result_final_faiss_1x1/')
   model_name_2 = './result_final_faiss_1x1/' + files[0]
