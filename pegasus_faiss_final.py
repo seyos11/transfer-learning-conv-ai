51c2818a865e78242fa51a9e06ea886e6cb39e60
victor <victor-gr7@hotmail.com>
2022-05-25 22:08:19 +0200

    final

--- pegasus_faiss_final.py
+++ pegasus_faiss_final.py
@@ -590,18 +590,18 @@ if __name__=='__main__':
   # use Pegasus Large model as base for fine-tuning
   model_name = 'google/pegasus-large'
 
-  train_dataset, valid_dataset, _, tokenizer = prepare_data(model_name, train_texts, train_labels,val_texts=valid_texts, val_labels=valid_labels)
-  trainer = prepare_fine_tuning(model_name, tokenizer, train_dataset,val_dataset=valid_dataset)
-  trainer.train()
+  #train_dataset, valid_dataset, _, tokenizer = prepare_data(model_name, train_texts, train_labels,val_texts=valid_texts, val_labels=valid_labels)
+  #trainer = prepare_fine_tuning(model_name, tokenizer, train_dataset,val_dataset=valid_dataset)
+  #trainer.train()
   
   files = os.listdir('result_final_normal_4x4/')
   model_name_1 = './result_final_normal_4x4/' + files[0]
   dataset = get_data_loaders_1sentence()
   train_texts, train_labels = dataset['train']['input_ids'], dataset['train']['decoder_input_ids']
   valid_texts, valid_labels = dataset['valid']['input_ids'], dataset['valid']['input_ids']
-  train_dataset, valid_dataset, _, tokenizer = prepare_data(model_name_1, train_texts, train_labels,val_texts=valid_texts, val_labels=valid_labels)
-  trainer2 = prepare_fine_tuning_faiss1x1(model_name_1, tokenizer, train_dataset,val_dataset=valid_dataset)
-  trainer2.train()
+  #train_dataset, valid_dataset, _, tokenizer = prepare_data(model_name_1, train_texts, train_labels,val_texts=valid_texts, val_labels=valid_labels)
+  #trainer2 = prepare_fine_tuning_faiss1x1(model_name_1, tokenizer, train_dataset,val_dataset=valid_dataset)
+  #trainer2.train()
   
   files = os.listdir('result_final_faiss_1x1/')
   model_name_2 = './result_final_faiss_1x1/' + files[0]
