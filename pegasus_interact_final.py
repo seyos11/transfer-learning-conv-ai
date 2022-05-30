59840bfe7f8cf98cd8ed83baee63a72bad806efe
victor <victor-gr7@hotmail.com>
2022-05-30 21:27:11 +0200

    itneract

--- pegasus_interact_final.py
+++ pegasus_interact_final.py
@@ -75,7 +75,6 @@ class PegasusDataset(torch.utils.data.Dataset):
         return item
     def __len__(self):
         return len(self.labels['input_ids'])  # len(self.labels)
-    
 def get_dataset(dataset_path, dataset_cache=None):
     """ Get PERSONACHAT from S3 """
     dataset_path = dataset_path or PERSONACHAT_URL
@@ -340,115 +339,139 @@ def run():
     print(metric_bleu.inputs_description)
     print(metric_rouge.inputs_description)
     print(metric_cosine_similarity.inputs_description)
-
+    prueba = 0
     #metric4x4.add_batch(predictions=predicted_tokens1, references=references)    
     #metric4x4.compute(predicionts=predicted_tokens1, references = references) 
-    count = 0
-    
-    def postprocess_text(preds, labels):
-        preds = [pred.split() for x in preds for pred in x]
-        #labels = [[label.split()] for label in labels]
-
-        return preds, labels
-    def postprocess_text2(preds, labels):
-        preds = [pred.strip() for x in preds for pred in x]
-        #labels = [[label.strip()] for label in labels]
-
-        return preds, labels
-    for i in tqdm(dataset['valid']['input_ids'][:30]):
-        batch = tokenizer(i, truncation=True, padding="longest", return_tensors="pt").to('cuda')
-        output = model.generate(**batch)
-        predicciones.append(output)
-    #for i in tqdm(dataset['valid']['input_ids'][:100]):
-        #print (i)
-    #batch = tokenizer(i, truncation=True, padding="longest", return_tensors="pt").to('cuda')
-    #batch2 = tokenizer(dataset['valid']['decoder_input_ids'][count],truncation=True, padding="longest", return_tensors="pt").to('cuda')
-    #output = model.generate(**batch)
     decoded_preds = []
     decoded_labels = []
-    for i in predicciones:
-        decoded_preds.append(tokenizer.batch_decode(i, skip_special_tokens=True))
-    for i in dataset['valid']['decoder_input_ids'][:30]:
-        decoded_labels.append([i.split()])
-    print(decoded_labels)
+    if (prueba == 1):
+        count = 0
+        
+        def postprocess_text(preds, labels):
+            preds = [pred.split() for x in preds for pred in x]
+            #labels = [[label.split()] for label in labels]
 
-    result1 = metric_bleu.compute(predictions=decoded_preds,references=decoded_labels)  
-    
-    decoded_preds, decoded_labels = postprocess_text(decoded_preds, decoded_labels)
-    decoded_labels = [" ".join(i) for j in decoded_labels for i in j]
-    decoded_preds = [" ".join(i) for i in decoded_preds]
-    #result2 = metric_rouge.compute(predictions=decoded_preds,references=decoded_labels)  
+            return preds, labels
+        def postprocess_text2(preds, labels):
+            preds = [pred.strip() for x in preds for pred in x]
+            #labels = [[label.strip()] for label in labels]
 
-    #decoded_preds = list(itertools.chain(*decoded_preds))
-    #print(predicciones)
-    #decoded_preds = tokenizer.batch_decode(predicciones, skip_special_tokens=True)
-    #decoded_labels = [dataset['valid']['decoder_input_ids'][:30]]
-    #decoded_preds, decoded_labels = postprocess_text(decoded_preds, decoded_labels)
-    #print(output)
-    #print(batch2)
-    #print(batch)
-    #predictedTokens4x4.append(model.generate(**batch))
-    print(decoded_preds)
-    print("decoded labels")
-    print(decoded_labels)
-    #predictedTokens4x4.append(decoded_preds)
-    #references.append(decoded_labels)
-        ##metric4x4.add(prediction=output, reference=batch2['input_ids'])    
-    #metric4x4 = load_metric('bleu')
-    #metric4x4.add_batch(predictions=predicted_tokens1, references=dataset['valid']['decoder_input_ids'])    
-    #result = metric4x4.compute(predictions=decoded_preds,references=decoded_labels)  
-    
-    print(result1)
-    decoded_preds = ["".join(decoded_preds)]
-    decoded_labels =["".join(decoded_labels)]
-    print(decoded_preds)
-    print("decoded labels")
-    print(decoded_labels)
-    result3 = metric_cosine_similarity.compute(predictions=decoded_preds,references=decoded_labels, lang='en')  
-    print(result3)
-    
-    model = SentenceTransformer('all-mpnet-base-v2')
-    embeddings_pred = model.encode(decoded_preds, show_progress_bar=False)   
-        # Step 1: Change data type
-    embeddings_pred = np.array([embedding for embedding in embeddings_pred]).astype("float32")
-    embeddings_label = model.encode(decoded_labels, show_progress_bar=False)   
-        # Step 1: Change data type
-    embeddings_label = np.array([embedding for embedding in embeddings_label]).astype("float32")
-    print(embeddings_pred)
-    print(cosine_similarity(embeddings_pred, embeddings_label))
-'''     dataset = get_data_loaders()        
-    predictedTokens1x1 = []  
-    for i in dataset['valid']['input_ids']:
-        batch = tokenizer(i, truncation=True, padding="longest", return_tensors="pt").to('cpu')
-        predictedTokens1x1.append(model.generate(**batch))
-    metric1x1 = load_metric('bleu')
-    metric1x1.compute(predicionts=predictedTokens1x1, references = dataset['valid']['decoder_input_ids'])
+            return preds, labels
+        for i in tqdm(dataset['valid']['input_ids'][:30]):
+            batch = tokenizer(i, truncation=True, padding="longest", return_tensors="pt").to('cuda')
+            output = model.generate(**batch)
+            predicciones.append(output)
+        #for i in tqdm(dataset['valid']['input_ids'][:100]):
+            #print (i)
+        #batch = tokenizer(i, truncation=True, padding="longest", return_tensors="pt").to('cuda')
+        #batch2 = tokenizer(dataset['valid']['decoder_input_ids'][count],truncation=True, padding="longest", return_tensors="pt").to('cuda')
+        #output = model.generate(**batch)
+
+        for i in predicciones:
+            decoded_preds.append(tokenizer.batch_decode(i, skip_special_tokens=True))
+        for i in dataset['valid']['decoder_input_ids'][:30]:
+            decoded_labels.append([i.split()])
+        print(decoded_labels)
+
+        result1 = metric_bleu.compute(predictions=decoded_preds,references=decoded_labels)  
+        
+        decoded_preds, decoded_labels = postprocess_text(decoded_preds, decoded_labels)
+        decoded_labels = [" ".join(i) for j in decoded_labels for i in j]
+        decoded_preds = [" ".join(i) for i in decoded_preds]
+        #result2 = metric_rouge.compute(predictions=decoded_preds,references=decoded_labels)  
+
+        #decoded_preds = list(itertools.chain(*decoded_preds))
+        #print(predicciones)
+        #decoded_preds = tokenizer.batch_decode(predicciones, skip_special_tokens=True)
+        #decoded_labels = [dataset['valid']['decoder_input_ids'][:30]]
+        #decoded_preds, decoded_labels = postprocess_text(decoded_preds, decoded_labels)
+        #print(output)
+        #print(batch2)
+        #print(batch)
+        #predictedTokens4x4.append(model.generate(**batch))
+        print(decoded_preds)
+        print("decoded labels")
+        print(decoded_labels)
+        #predictedTokens4x4.append(decoded_preds)
+        #references.append(decoded_labels)
+            ##metric4x4.add(prediction=output, reference=batch2['input_ids'])    
+        #metric4x4 = load_metric('bleu')
+        #metric4x4.add_batch(predictions=predicted_tokens1, references=dataset['valid']['decoder_input_ids'])    
+        #result = metric4x4.compute(predictions=decoded_preds,references=decoded_labels)  
+        
+        print(result1)
+        decoded_preds = ["".join(decoded_preds)]
+        decoded_labels =["".join(decoded_labels)]
+        print(decoded_preds)
+        print("decoded labels")
+        print(decoded_labels)
+        result3 = metric_cosine_similarity.compute(predictions=decoded_preds,references=decoded_labels, lang='en')  
+        print(result3)
+        
+        model = SentenceTransformer('all-mpnet-base-v2')
+        embeddings_pred = model.encode(decoded_preds, show_progress_bar=False)   
+            # Step 1: Change data type
+        embeddings_pred = np.array([embedding for embedding in embeddings_pred]).astype("float32")
+        embeddings_label = model.encode(decoded_labels, show_progress_bar=False)   
+            # Step 1: Change data type
+        embeddings_label = np.array([embedding for embedding in embeddings_label]).astype("float32")
+        #print(embeddings_pred)
+        result = cosine_similarity(embeddings_pred, embeddings_label)
+        
+        
+        '''     dataset = get_data_loaders()        
+        predictedTokens1x1 = []  
+        for i in dataset['valid']['input_ids']:
+            batch = tokenizer(i, truncation=True, padding="longest", return_tensors="pt").to('cpu')
+            predictedTokens1x1.append(model.generate(**batch))
+        metric1x1 = load_metric('bleu')
+        metric1x1.compute(predicionts=predictedTokens1x1, references = dataset['valid']['decoder_input_ids'])
 
 
-    dataset = get_data_loaders_2sentences()     
-    predictedTokens2x2 = []  
-    for i in dataset['valid']['input_ids']:
-        batch = tokenizer(i, truncation=True, padding="longest", return_tensors="pt").to('cpu')
-        predictedTokens2x2.append(model.generate(**batch))
-    metric2x2 = load_metric('bleu')
-    metric2x2.compute(predicionts=predictedTokens2x2, references = dataset['valid']['decoder_input_ids'])
+        dataset = get_data_loaders_2sentences()     
+        predictedTokens2x2 = []  
+        for i in dataset['valid']['input_ids']:
+            batch = tokenizer(i, truncation=True, padding="longest", return_tensors="pt").to('cpu')
+            predictedTokens2x2.append(model.generate(**batch))
+        metric2x2 = load_metric('bleu')
+        metric2x2.compute(predicionts=predictedTokens2x2, references = dataset['valid']['decoder_input_ids'])
 
-    dataset = get_data_loaders_3sentences()       
-    predictedTokens3x3 = []  
-    for i in dataset['valid']['input_ids']:
-        batch = tokenizer(i, truncation=True, padding="longest", return_tensors="pt").to('cpu')
-        predictedTokens3x3.append(model.generate(**batch))
-    metric3x3 = load_metric('bleu')
-    metric3x3.compute(predicionts=predictedTokens3x3, references = dataset['valid']['decoder_input_ids'])
-    print(metric1x1)
-    print(metric2x2) '''
-''' def compute_metrics(eval_pred):
-    
-    logits, labels = eval_pred
+        dataset = get_data_loaders_3sentences()       
+        predictedTokens3x3 = []  
+        for i in dataset['valid']['input_ids']:
+            batch = tokenizer(i, truncation=True, padding="longest", return_tensors="pt").to('cpu')
+            predictedTokens3x3.append(model.generate(**batch))
+        metric3x3 = load_metric('bleu')
+        metric3x3.compute(predicionts=predictedTokens3x3, references = dataset['valid']['decoder_input_ids'])
+        print(metric1x1)
+        print(metric2x2) '''
+        
+        ''' def compute_metrics(eval_pred):
+        
+        logits, labels = eval_pred
 
-    predictions = np.argmax(logits, axis=-1)
+        predictions = np.argmax(logits, axis=-1)
 
-    return metric.compute(predictions=predictions, references=labels)) '''
-
+        return metric.compute(predictions=predictions, references=labels)) '''
+             
+        
+    else:
+        import spacy
+        nlp = spacy.load('en_core_web_sm')
+        
+        for i in tqdm(dataset['valid']['input_ids'][:10]):
+            batch = tokenizer(i, truncation=True, padding="longest", return_tensors="pt").to('cuda')
+            output = model.generate(**batch)
+            predicciones.append(output)
+        print(predicciones)
+        
+        for i in predicciones:
+            decoded_preds.append(tokenizer.batch_decode(i, skip_special_tokens=True))
+        for i in dataset['valid']['decoder_input_ids'][:10]:
+            decoded_labels.append([i])
+        sentences = [i for i in nlp(predictions).sents]
+        print(sentences)
+        references = [i for i in nlp(labels).sents]   
+        personalities = [i for i in nlp(personality_set).sents]                     
 if __name__ == "__main__":
     run()
